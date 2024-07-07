5995a361a020e554ee097770d2bd3d0cbd333f73
Krzysztof Pisz <krzysiek.pisz@gmail.com>
2024-07-07 07:38:36 +0200

    Update tamperyt.js

--- tamperyt.js
+++ tamperyt.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TamerYT
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/watch?v=*
@@ -75,7 +75,7 @@
         }
         setTimeout(function(){
             getData().then(x => blockYt(x))
-        }, 500)
+        }, 1500)
     }
 
     console.log('Staring')
